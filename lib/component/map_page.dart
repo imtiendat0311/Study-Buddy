b516e12267abf1791a9cb1c71eb38653670fad0d
superboo0311 <60676245+superboo0311@users.noreply.github.com>
2023-04-18 19:31:30 -0400

    adding google map

--- lib/component/map_page.dart
+++ lib/component/map_page.dart
@@ -1,11 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-class MapPage extends StatelessWidget {
+class MapPage extends StatefulWidget {
   final void Function({String returnValue}) onClosed;
 
   const MapPage({super.key, required this.onClosed});
+  State<MapPage> createState() => _MapPageState();
+}
+
+class _MapPageState extends State<MapPage> {
+  late GoogleMapController mapController;
+
+  final LatLng _center = const LatLng(45.521563, -122.677433);
+
+  void _onMapCreated(GoogleMapController controller) {
+    mapController = controller;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -13,11 +25,20 @@ class MapPage extends StatelessWidget {
       appBar: AppBar(
         title: Text("Map"),
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          widget.onClosed(returnValue: "Hello");
+        },
+        child: Icon(Icons.check),
+      ),
       body: SafeArea(
-        child: Column(children: [
-          Text("implement google map here"),
-          FilledButton.tonal(onPressed: onClosed, child: Text("OKE"))
-        ]),
+        child: GoogleMap(
+          onMapCreated: _onMapCreated,
+          initialCameraPosition: CameraPosition(
+            target: _center,
+            zoom: 11.0,
+          ),
+        ),
       ),
     );
   }
