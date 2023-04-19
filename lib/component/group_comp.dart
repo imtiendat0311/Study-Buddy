5ccc725911bc56e42f2eaac782657fc32620a7be
superboo0311 <60676245+superboo0311@users.noreply.github.com>
2023-04-19 13:01:16 -0400

    adding list view group

--- lib/component/group_comp.dart
+++ lib/component/group_comp.dart
@@ -1,8 +1,8 @@
-import 'package:animations/animations.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:study_buddy/component/search_bar.dart';
-import 'package:study_buddy/component/search_page.dart';
+
 import 'package:study_buddy/component/search_result_page.dart';
 import 'package:study_buddy/scene/create_group.dart';
 
@@ -14,6 +14,8 @@ class GroupComp extends StatefulWidget {
 }
 
 class _GroupCompState extends State<GroupComp> {
+  final db = FirebaseFirestore.instance;
+  final auth = FirebaseAuth.instance;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -57,7 +59,66 @@ class _GroupCompState extends State<GroupComp> {
                   const Text(
                     "Available Groups",
                     style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                  )
+                  ),
+                  StreamBuilder(
+                      stream: db.collection("groups").snapshots(),
+                      builder: (context, snapshot) {
+                        if (snapshot.hasData && snapshot.data != null) {
+                          if (snapshot.data!.docs.isEmpty) {
+                            return Center(
+                              child: Card(
+                                  shadowColor: Colors.black.withOpacity(0.5),
+                                  elevation: 2,
+                                  child: const SizedBox(
+                                      height: 100,
+                                      width: 400,
+                                      child: Center(
+                                        child: Text(
+                                          "No event",
+                                          textAlign: TextAlign.center,
+                                        ),
+                                      ))),
+                            );
+                          } else {
+                            return ListView.builder(
+                                shrinkWrap: true,
+                                itemCount: snapshot.data!.docs.length,
+                                itemBuilder: (context, index) {
+                                  return Padding(
+                                    padding: const EdgeInsets.all(8.0),
+                                    child: FilledButton.tonal(
+                                        style: ButtonStyle(
+                                            shape: MaterialStateProperty.all<
+                                                RoundedRectangleBorder>(
+                                          RoundedRectangleBorder(
+                                            borderRadius:
+                                                BorderRadius.circular(15.0),
+                                          ),
+                                        )),
+                                        onPressed: () {},
+                                        child: SizedBox(
+                                          child: Column(
+                                            children: [
+                                              Text(snapshot.data!.docs[index]
+                                                  ["title"]),
+                                              Text(snapshot.data!.docs[index]
+                                                  ["course"]),
+                                              Text(snapshot.data!.docs[index]
+                                                  ["location"]),
+                                              Text(
+                                                  "Member : ${snapshot.data!.docs[index]['members'].length}")
+                                            ],
+                                          ),
+                                        )),
+                                  );
+                                });
+                          }
+                        } else if (snapshot.hasError) {
+                          return const Text('Error');
+                        } else {
+                          return const CircularProgressIndicator();
+                        }
+                      })
                 ],
               ))),
     );
